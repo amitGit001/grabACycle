2ffe24f50ab5ea485c4c8da6d9b93e1f07946d35
Jayasyam <121284762+Jayasyam-K@users.noreply.github.com>
2024-02-09 13:41:19 +0530

    Added log4j and made the appropriate changes , the log messages can be seen on console

--- src/main/java/com/grabACycle/grabACycle/GrabACycleApplication.java
+++ src/main/java/com/grabACycle/grabACycle/GrabACycleApplication.java
@@ -1,5 +1,7 @@
 package com.grabACycle.grabACycle;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -7,8 +9,13 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 public class GrabACycleApplication {
+	private static final Logger logger = LogManager.getLogger(GrabACycleApplication.class);
+
 
 	public static void main(String[] args) {
+		logger.info("This is a info log message");
+		logger.debug("This is a debug message");
+		logger.warn("This is Warning message");
 		SpringApplication.run(GrabACycleApplication.class, args);
 	}
 
