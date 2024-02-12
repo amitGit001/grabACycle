46067f7e20175de46c62e7db78180486bcbe3b8a
Siddharth Kumar <40791172+ryukirisame@users.noreply.github.com>
2024-02-12 18:41:01 +0530

    v1.0 release

--- src/main/java/com/grabACycle/grabACycle/controller/CycleController.java
+++ src/main/java/com/grabACycle/grabACycle/controller/CycleController.java
@@ -7,6 +7,8 @@ import com.grabACycle.grabACycle.exception.messages.CycleErrorResponse;
 import com.grabACycle.grabACycle.services.CycleService;
 import com.grabACycle.grabACycle.web.dto.CycleDto;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +26,8 @@ public class CycleController
     static int PAGE_SIZE=20;
     private CycleService cycleService;
 
+    private static final Logger logger = LoggerFactory.getLogger(CycleController.class);
+
     @Autowired
     public CycleController(CycleService cycleService)
     {
@@ -35,20 +39,28 @@ public class CycleController
     @GetMapping("/")
     public String viewHomePage(Model model)
     {
+        logger.info("Request received to view home page");
         return findPaginated(1, "id","asc",model); // default page number=1
     }
     @GetMapping("/page/{pageNo}")
     public String findPaginated(@PathVariable(value="pageNo") int pageNo, @RequestParam("sortField")  String sortField, @RequestParam("sortDir") String sortDir, Model model)
     {
+        logger.info("Request received to find paginated list of cycles. Page number: {}, Sort Field: {}, Sort Direction: {}", pageNo, sortField, sortDir);
+
         if (pageNo <= 0) {
+            logger.error("Invalid page number received: {}", pageNo);
             throw new InvalidPageNumException("Page number must be greater than 0");
         }
 
-        if (!(sortField.equals("id") || sortField.equals("bookingStatus") || sortField.equals("model") || sortField.equals("name") || sortField.equals("type") || sortField.equals("bookedBy")))
+        if (!(sortField.equals("id") || sortField.equals("bookingStatus") || sortField.equals("model") || sortField.equals("name") || sortField.equals("type") || sortField.equals("bookedBy"))){
+            logger.error("Invalid sort field received: {}", sortField);
             throw new InvalidSortFieldException("Invalid sort field: " + sortField);
+        }
+
 
         if(!(sortDir.equals("asc") || sortDir.equals("desc")))
         {
+            logger.error("Invalid sort direction received: {}", sortDir);
             throw new InvalidSortDirException("Invalid sort Direction: "+sortDir);
         }
 
@@ -58,6 +70,7 @@ public class CycleController
         Page<Cycle> page=cycleService.findPaginated(pageNo, pageSize,sortField, sortDir);
         int totalPages=page.getTotalPages();
         if (pageNo > totalPages) {
+            logger.error("Page number exceeds total pages. Received: {}, Total Pages: {}", pageNo, totalPages);
             throw new InvalidPageNumException("Page number exceeds total pages");
         }
 
@@ -80,19 +93,28 @@ public class CycleController
     @GetMapping("/cycles/page/{pageNo}")
     @ResponseBody
     public CycleDto fetchPage(@PathVariable(value="pageNo") int pageNo, @RequestParam("sortField") String sortField, @RequestParam("sortDir") String sortDir){
+        logger.info("Request received to fetch page. Page number: {}, Sort Field: {}, Sort Direction: {}", pageNo, sortField, sortDir);
 
 
         int pageSize= PAGE_SIZE; // we can take the pagesize from front end as well using path variable, or declare the size in application.properties
 
         if (pageNo <= 0) {
+            logger.error("Invalid page number received: {}", pageNo);
+
             throw new InvalidPageNumException("Page number must be greater than 0");
         }
 
-        if (!(sortField.equals("id") || sortField.equals("bookingStatus") || sortField.equals("model") || sortField.equals("name") || sortField.equals("type") || sortField.equals("bookedBy")))
+        if (!(sortField.equals("id") || sortField.equals("bookingStatus") || sortField.equals("model") || sortField.equals("name") || sortField.equals("type") || sortField.equals("bookedBy"))){
+            logger.error("Invalid sort field received: {}", sortField);
             throw new InvalidSortFieldException("Invalid sort field: " + sortField);
+
+        }
+
 
         if(!(sortDir.equals("asc") || sortDir.equals("desc")))
         {
+            logger.error("Invalid sort direction received: {}", sortDir);
+
             throw new InvalidSortDirException("Invalid sort Direction: "+sortDir);
         }
 
@@ -100,6 +122,8 @@ public class CycleController
 
         int totalPages=page.getTotalPages();
         if (pageNo > totalPages) {
+            logger.error("Page number exceeds total pages. Received: {}, Total Pages: {}", pageNo, totalPages);
+
             throw new InvalidPageNumException("Page number exceeds total pages");
         }
 
@@ -116,6 +140,7 @@ public class CycleController
     @ResponseBody
     public CycleDto deleteCycle(@PathVariable(value="id") int cycleId, @RequestParam("page") int pageNo, @RequestParam("sortField") String sortField, @RequestParam("sortDir") String sortDir)
     {
+        logger.info("Request received to delete cycle with ID: {}", cycleId);
 
         try {
             // call delete cycle method
@@ -123,6 +148,8 @@ public class CycleController
         }
         catch (Exception e)
         {
+            logger.error("Cycle with ID {} not found", cycleId);
+
             throw new CycleNotFoundException("Cycle with ID " + cycleId + " could not be deleted.");
         }
         return fetchPage(pageNo, sortField, sortDir);
@@ -135,15 +162,25 @@ public class CycleController
                                    @RequestParam(value="sortDir") String sortDir,
                                    Model model)
     {
+        logger.info("Request received to show new cycle form.");
+
         if (page <= 0) {
+            logger.error("Invalid value for returnToPage parameter: {}", page);
+
             throw new InvalidPageNumException("Invalid value for returnToPage parameter");
         }
 
         if (!(sortField.equals("id") || sortField.equals("bookingStatus") || sortField.equals("model") || sortField.equals("name") || sortField.equals("type") || sortField.equals("bookedBy")))
+        {
+            logger.error("Sort field parameter is missing or empty");
             throw new InvalidSortFieldException("Invalid sort field: " + sortField);
+        }
+
 
         if(!(sortDir.equals("asc") || sortDir.equals("desc")))
         {
+            logger.error("Invalid sort direction parameter: {}", sortDir);
+
             throw new InvalidSortDirException("Invalid sort Direction: "+sortDir);
         }
 
@@ -163,12 +200,14 @@ public class CycleController
                             @RequestParam(value = "sortDir") String sortDir,
                             @RequestParam(value = "returnToPage") int page)
     {
-
+        logger.info("Request received to save new cycle ");
 
         try {
             // Save cycle to database
             cycleService.createCycle(cycle);
         } catch (Exception e) {
+            logger.error("Error occurred while saving the cycle", e);
+
             throw new CycleCreationException("Error occurred while saving the cycle");
         }
 
@@ -187,6 +226,8 @@ public class CycleController
                                     @Valid Model model)
     {
 
+        logger.info("Request received to show update cycle form for ID: {}", id);
+
         if (!(sortField.equals("id") || sortField.equals("bookingStatus") || sortField.equals("model") || sortField.equals("name") || sortField.equals("type") || sortField.equals("bookedBy")))
             throw new InvalidSortFieldException("Invalid sort field: " + sortField);
 
@@ -198,7 +239,9 @@ public class CycleController
         Cycle cycle=null;
         try {
             // get cycle from the service layer
-         cycle = cycleService.fetchCycleById(id);
+            cycle = cycleService.fetchCycleById(id);
+            logger.info("Cycle details retrieved for update: {}", cycle);
+
             // set cycle as a model attribute to pre-populate the form data
             model.addAttribute("cycle", cycle);
             model.addAttribute("returnToPage", page);
@@ -207,6 +250,8 @@ public class CycleController
         }
         catch (Exception e)
         {
+            logger.error("Cycle with ID {} not found", id);
+
             throw new CycleNotFoundException("Cycle does not exist");
         }
 
@@ -218,14 +263,20 @@ public class CycleController
         @ResponseBody
         public Cycle updateBookingStatus(@PathVariable int cycleId, Principal principal){
 
+            logger.info("Request received to update booking status for cycle with ID: {}", cycleId);
+
             String userEmail = principal.getName();
 
           Cycle cycle = null;
 
           try {
-            cycle =  cycleService.updateBookingStatus(cycleId, userEmail);
+              logger.info("Updating booking status for cycle with ID: {} is successful", cycleId);
+
+              cycle =  cycleService.updateBookingStatus(cycleId, userEmail);
           }catch (Exception e)
           {
+              logger.error("Cycle with ID {} not found", cycleId);
+
               throw new UpdateBookingStatusException("Booking Failed");
           }
           return cycle;
